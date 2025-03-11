7232de3201a439ad2085fc5fa8e701da473ce445
ANGEL_TADEO_DE_LEON_CENICEROS <a.22308051281165@cbtis128.edu.mx>
2025-03-11 15:26:27 +0000

    Pusheando cambios 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisListViews());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisListViews extends StatelessWidget {
+  const MisListViews({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("ListView De Leon 1165"),
+          titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
+          centerTitle: true,
+          backgroundColor: const Color(0xff36b6f1),
+        ),
+        body: ListView(
+          children: const [
+            ListTile(
+              leading: Icon(Icons.battery_full, color: Colors.grey),
+              title: Text("Bateria Llena",
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              subtitle: Text("La bateria esta completa."),
+              trailing: Icon(Icons.star_border),
+            ),
+            ListTile(
+              leading: Icon(Icons.anchor, color: Colors.grey),
+              title:
+                  Text("Ancla", style: TextStyle(fontWeight: FontWeight.bold)),
+              subtitle: Text("Baja el ancla. (arrr!)"),
+              trailing: Icon(Icons.star_border),
+            ),
+            ListTile(
+              leading: Icon(Icons.alarm, color: Colors.grey),
+              title:
+                  Text("Alarma", style: TextStyle(fontWeight: FontWeight.bold)),
+              subtitle: Text("Esta es la hora."),
+              trailing: Icon(Icons.star_border),
+            ),
+            ListTile(
+              leading: Icon(Icons.ballot, color: Colors.grey),
+              title: Text("Votacion",
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              subtitle: Text("Haz valer tu voto."),
+              trailing: Icon(Icons.star_border),
+            ),
+          ],
         ),
       ),
     );
